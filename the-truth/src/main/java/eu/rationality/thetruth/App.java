b0f73737185039a982f781cb1eb6e37df9372129
Simon Schuster <git@rationality.eu>
2018-09-02 13:32:11 +0200

    Generate initial maven prototype

---
+++ the-truth/src/main/java/eu/rationality/thetruth/App.java
@@ -0,0 +1,13 @@
+package eu.rationality.thetruth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
